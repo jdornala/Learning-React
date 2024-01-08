a6ba53cb56c0c2528e546ab4aef2dfc514ab2c85
Jagadeeshwar Dornala <jdornala@in.ibm.com>
2024-01-08 11:35:59 +0530

    Episode 03

--- App.js
+++ App.js
@@ -5,19 +5,37 @@ import  ReactDOM  from "react-dom/client"
 
 // To construct above html follow below code
 
-const  parent = React.createElement("div", {id:"parent"}, 
-                    React.createElement("div", {id:"child"}, [
-                            React.createElement("h1", {}, "I'm a H1 tag"), 
-                            React.createElement("h2", {}, "I'm a H2 tag")
-                        ]
-                        )
-                        )
+// const  parent = React.createElement("div", {id:"parent"}, 
+//                     React.createElement("div", {id:"child"}, [
+//                             React.createElement("h1", {}, "I'm a H1 tag"), 
+//                             React.createElement("h2", {}, "I'm a H2 tag")
+//                         ]
+//                         )
+//                         )
 
 
 
 
 // const heading = React.createElement("h1", { id:"heading" }, "Hellow world from React");
 
+const JSXHeadingComponent = () => { 
+    return   <h1 id="heading">  
+    I'm JSX in React 
+    </h1>
+}
+
+const FunctionalComponent =() => (
+    <div id="container">
+        <JSXHeadingComponent/>
+        <JSXHeadingComponent></JSXHeadingComponent>
+        {JSXHeadingComponent()}
+        {100 + 100}
+        <h1>I'm a Functionlal Component</h1>
+    </div>
+);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<FunctionalComponent />);
